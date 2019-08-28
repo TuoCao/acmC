5424c94f934f72d1d54fb25506cccc2ae8525e64
TuoCao <2587593291@qq.com>
2019-08-28 18:13:55 +0800

    no message

--- addTwoNumbers.c
+++ addTwoNumbers.c
@@ -7,17 +7,16 @@ struct ListNode {
 };
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-	struct ListNode* ans = NULL;
-	struct ListNode* t = ans;
+	struct ListNode head = { 0,NULL };
+	struct ListNode* t = &head;
 	int temp = 0;
 	int ac = 0;
 	while (l1 != NULL && l2 != NULL) {
-		t = (struct ListNode*)malloc(sizeof(struct ListNode));
-
+		t->next = (struct ListNode*)malloc(sizeof(struct ListNode));
 		temp = l1->val + l2->val + ac;
-		t->val = temp / 10;
-		ac = temp % 10;
-		
+		t->next->val = temp % 10;
+		ac = temp / 10;
+
 		t = t->next;
 		l1 = l1->next;
 		l2 = l2->next;
@@ -28,34 +27,40 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
 	}
 
 	while (l1 != NULL) {
-		t = (struct ListNode*)malloc(sizeof(struct ListNode));
-		t = t->next;
-		temp = l1->val + ac;
-		t->val = temp / 10;
-		ac = temp % 10;
+		t->next = (struct ListNode*)malloc(sizeof(struct ListNode));
 		
+		temp = l1->val + ac;
+		t->next->val = temp % 10;
+		ac = temp / 10;
+		t = t->next;
 		l1 = l1->next;
 	}
 	if (ac == 1) {
-		t = (struct ListNode*)malloc(sizeof(struct ListNode));
-		t->next = NULL;
-		t->val = 1;
+		t->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+		t->next->val = 1;
+		t->next->next = NULL;
 	}
 	else {
-		t = NULL;
+		t->next = NULL;
 	}
-	
-	return ans;
+	return head.next;
 }
 
-void initListNode(struct ListNode* ln, int* nums) {
-	int size = sizeof(nums) / sizeof(int);
-	for (int i = 0; i < size; i++) {
-		ln = malloc(sizeof(struct ListNode));
-		ln->val = nums[i];
-		ln->next = NULL;
+void showListNode(struct ListNode* ln) {
+	while (ln != NULL) {
+		printf("%d", ln->val);
 		ln = ln->next;
 	}
+}
+
+void initListNode(struct ListNode* ln, int* nums,int size) {
+	struct ListNode* t = ln;
+	for (int i = 0; i < size; i++) {
+		t->next = malloc(sizeof(struct ListNode));
+		t->next->val = nums[i];
+		t = t->next;
+	}
+	t->next = NULL;
 }
 
 void freeListNode(struct ListNode* ln) {
@@ -67,26 +72,19 @@ void freeListNode(struct ListNode* ln) {
 	}
 }
 
-void showListNode(struct ListNode* ln) {
-	while (ln != NULL) {
-		printf("%d", ln->val);
-		ln = ln->next;
-	}
-}
-
 int main() {
 	int nums1[] = { 2,4,3 };
 	int nums2[] = { 5,6,4 };
-	struct ListNode* l1 = NULL;
-	struct ListNode* l2 = NULL;
-	struct ListNode* res = NULL;
-	initListNode(l1, nums1);
-	initListNode(l2, nums2);
-	res = addTwoNumbers(l1, l2);
+	struct ListNode l1 = {0,NULL};
+	struct ListNode l2 = {0,NULL};
+	struct ListNode* res;
+	initListNode(&l1, nums1,sizeof(nums1)/sizeof(int));
+	initListNode(&l2, nums2,sizeof(nums2)/sizeof(int));
+	res = addTwoNumbers(l1.next, l2.next);
 	showListNode(res);
 
-	freeListNode(l1);
-	freeListNode(l2);
+	freeListNode(l1.next);
+	freeListNode(l2.next);
 	freeListNode(res);
 	return 0;
 }
